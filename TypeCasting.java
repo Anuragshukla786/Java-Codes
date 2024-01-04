3eddd6c2d6adacb7951a58617a84622e623a9a56
Anuragshukla786 <shuklaweb786@gmail.com>
2024-01-04 21:15:08 +0530

    Some changes

--- TypeCasting.java
+++ TypeCasting.java
@@ -1,10 +1,26 @@
 public class TypeCasting {
     public static void main(String[] args) {
-        
+       // implicit TypeCasting Because  compiler change 
+       //by itself 
         int a=45;
         double b;
         b=a;
         System.out.println(b);
+
+        // Explicit typecasting This casting is perform by
+        //a develoer
+        double  num1=4.55;
+        int num2;
+        num2=(int)num1;
+        System.out.println(num2);
+
+        // Always integer divide by integer Answer will be
+        //Integer
+        int n1=12;
+        int n2=5;
+        float res=n1/n2;
+        System.out.println(res);
+
     }
     
 }

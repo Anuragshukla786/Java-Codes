469c8a6cefcb5fc81ecd4a4ad07c6de7047412fd
Anuragshukla786 <shuklaweb786@gmail.com>
2024-01-08 16:20:51 +0530

    New Code

---
+++ LocalVariable.java
@@ -0,0 +1,25 @@
+public class LocalVariable {
+     //instance variable
+    int b;
+    String name ;
+
+    public static void main(String[] args) {
+        //declare the variable
+        //create the object
+
+        //Calling Istance Varivble
+        LocalVariable obj2=new LocalVariable();
+
+        System.out.println(obj2.b);
+        System.out.println(obj2.name);
+
+        //local variable is always decalre
+        int a=2;
+        String num="anu";
+        System.out.println(a);
+        System.out.println(num);
+
+      
+    }
+    
+}
---
+++ Test.java
@@ -0,0 +1,27 @@
+//Working of Instance Variable
+public class Test {
+    int a ;
+    String name ;
+
+    public static void main(String[] args) {
+        //declare the variable
+        //create the object
+
+        int num=9;//Primitive variable wo hote hai
+        //jo kisi value ko refer krte hai
+        Test obj=new Test();//Refrence variable wo hote hai jo kisi object ko 
+        //refer krte hai
+        obj.name="Anurag";
+       
+        System.out.println(obj.a);
+        System.out.println(obj.name);
+
+        Test obj2=new Test();
+
+        System.out.println(obj2.a);
+        System.out.println(obj2.name);
+        
+        
+    }
+    
+}
